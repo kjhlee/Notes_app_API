e9683191ca4922935b51bba5e950c19cfed755a5
theemotionalpie <kunnyjlee@gmail.com>
2023-07-02 18:02:19 -0700

    create main.py
    be able to create notes in python now

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from notes import *
+from models import Notes, authorModel
+import asyncio
+import uvicorn
+import json
+
+mainApp = FastAPI()
+
+mainApp.mount("/", app)
+
+async def createNewNote():
+    newText = input("this is a new note that I would like to add")
+    createNewNote = Notes(
+        title = "text2",
+        text = newText,
+        locked = True,
+        author = authorModel(
+            username = "pipin",
+            name = "KJ",
+            email = "urmom@gmail.com",
+            state = "active"
+        )
+    )
+    await createNote(createNewNote)
+
+
+async def main():
+    var = input("What would you like to do? ")
+    if var == "create_new":
+        await createNewNote()
+
+asyncio.run(main())
+uvicorn.run(mainApp)
